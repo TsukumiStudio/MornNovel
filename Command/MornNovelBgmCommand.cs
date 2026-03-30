bbe84ce4e31cb53b6c7afc7dfd731433b26ad7a5
matsufriends <pikapikan0526@gmail.com>
2026-03-31 02:45:19 +0900

    MornBeatの型名変更に追従 (MornBeatMemoSo→MornBeatMusic, MornBeatControllerMono→MornBeatController)

--- Command/MornNovelBgmCommand.cs
+++ Command/MornNovelBgmCommand.cs
@@ -9,21 +9,21 @@ namespace MornLib
     internal class MornNovelBgmCommand : MornNovelCommandBase
     {
         public override string Tips => "BGMを再生する(nullで停止)";
-        [SerializeField] private MornBeatMemoSo _beatMemo;
+        [SerializeField] private MornBeatMusic _beatMusic;
         [SerializeField] private StateLink _onComplete;
-        [Inject] private MornBeatControllerMono _beatController;
+        [Inject] private MornBeatController _beatController;
         [Inject] private MornNovelSettings _novelSettings;
 
         public override async void OnStateBegin()
         {
             // Stateに紐づかず、独立で稼働
             var ct = destroyCancellationToken;
-            if (_beatMemo != null)
+            if (_beatMusic != null)
             {
                 await _beatController.StartAsync(
                     new MornBeatStartInfo
                     {
-                        BeatMemo = _beatMemo,
+                        Music = _beatMusic,
                         FadeDuration = _novelSettings.BgmChangeSec,
                         Ct = ct,
                     });
