8bd4a47565c98ab068025e7f5d4f82b577e8778b
matsufriends <pikapikan0526@gmail.com>
2026-04-11 16:43:38 +0900

    MornColor依存を削除しTalkerSoにColor/Gradientを直接埋め込み
    MornNovelTalkerSoで使っていた MornColorInfo / MornGradientInfo の
    ScriptableObject参照を廃止し、Color/Gradient フィールドとして直接保持
    する形に変更。MornNovel.asmdef からも MornColor 参照を除去。

--- ScriptableObject/MornNovelTalkerSo.cs
+++ ScriptableObject/MornNovelTalkerSo.cs
@@ -1,5 +1,4 @@
-﻿using MornColor;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace MornLib
 {
@@ -8,19 +7,19 @@ namespace MornLib
     {
         [SerializeField] [Label("日本語")] private MornLocalizeString _localize;
         [SerializeField] [Label("複数人")] private bool _isMulti;
-        [SerializeField] [Label("文字色")] private MornColorInfo _textColor;
-        [SerializeField] [Label("名前グラデーション")] private MornGradientInfo _nameGradient;
-        [SerializeField] [Label("吹き出しフチ色/文字送り色")] private MornColorInfo _edgeColor;
+        [SerializeField] [Label("文字色")] private Color _textColor = Color.white;
+        [SerializeField] [Label("名前グラデーション")] private Gradient _nameGradient = new Gradient();
+        [SerializeField] [Label("吹き出しフチ色/文字送り色")] private Color _edgeColor = Color.white;
         [SerializeField] private AudioClip[] _clip;
         [SerializeField] private float _clipLength;
         public bool IsMulti => _isMulti;
-        public Color CommandColor => _edgeColor.Color;
-        public Color TextColor => _textColor.Color;
-        public Color NameBackTopGradientColor => _nameGradient.Gradient.Evaluate(0);
-        public Color NameBackCenterGradientColor => _nameGradient.Gradient.Evaluate(0.5f);
-        public Color NameBackBottomGradientColor => _nameGradient.Gradient.Evaluate(1);
-        public Color BubbleEdgeColor => _edgeColor.Color;
-        public Color MessageIconColor => _edgeColor.Color;
+        public Color CommandColor => _edgeColor;
+        public Color TextColor => _textColor;
+        public Color NameBackTopGradientColor => _nameGradient.Evaluate(0);
+        public Color NameBackCenterGradientColor => _nameGradient.Evaluate(0.5f);
+        public Color NameBackBottomGradientColor => _nameGradient.Evaluate(1);
+        public Color BubbleEdgeColor => _edgeColor;
+        public Color MessageIconColor => _edgeColor;
         public AudioClip[] Clips => _clip;
         public float ClipLength => _clipLength;
         public string GetText(string language) => _localize.Get(language);
